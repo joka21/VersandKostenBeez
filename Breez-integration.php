0697575ee58c1a905cbdd795d525eb30a3e4afb9
DrAlli (Marius Martin) <35598714+DrAlliteration@users.noreply.github.com>
2023-03-16 22:59:37 +0100

    Test#4

--- Breez-integration.php
+++ Breez-integration.php
@@ -18,7 +18,19 @@ if ( ! class_exists( 'Versand_Kosten_Beez_Integration' ) ) :
             $this->init_settings();
 
             // Define user set variables.
-            $this->custom_name          = $this->get_option( 'custom_name' );
+            $this->spritpreis = $this->get_option( 'spritpreis' );
+            $this->spritverbrauch = $this->get_option( 'spritverbrauch' );
+            $this->lohnkosten = $this->get_option( 'lohnkosten' );
+            $this->abschreibung_pro_km = $this->get_option( 'abschreibung_pro_km' );
+            $this->wartungskosten = $this->get_option( 'wartungskosten' );
+
+            // Test bezüglich der Berechnung
+            $this->plz1 = $this->get_option( 'plz1_input' );
+            $this->plz2 = $this->get_option( 'plz2_input' );
+            $this->entfernung_output = $this->get_option( 'entfernung_output' );
+            $this->fahrzeit_output = $this->get_option( 'fahrzeit_output' );
+            $this->kosten_output = $this->get_option( 'kosten_output' );
+
 
             // Actions.
             add_action( 'woocommerce_update_options_integration_' .  $this->id, array( $this, 'process_admin_options' ) );
@@ -30,14 +42,87 @@ if ( ! class_exists( 'Versand_Kosten_Beez_Integration' ) ) :
         */
         public function init_form_fields() {
             $this->form_fields = array(
-                'custom_name ' => array(
-                    'title'             => __( 'Versandkosten für Krefeld und Münster'),
-                    'type'              => 'text',
-                    'description'       => __( 'Test' ),
+                'spritpreis' => array(
+                    'title'             => __( 'Spritpreis'),
+                    'type'              => 'number',
+                    'description'       => __( 'Aktueller Spritpreis' ),
                     'desc_tip'          => true,
                     'default'           => '',
                     'css'      => 'width:170px;',
                 ),
+                'spritverbrauch' => array(
+                    'title'             => __( 'Spritverbrauch'),
+                    'type'              => 'number',
+                    'description'       => __( '' ),
+                    'desc_tip'          => false,
+                    'default'           => '',
+                    'css'      => 'width:170px;',
+                ),
+                'lohnkosten' => array(
+                    'title'             => __( 'Lohnkosten pro Stunde'),
+                    'type'              => 'text',
+                    'description'       => __( '' ),
+                    'desc_tip'          => false,
+                    'default'           => '',
+                    'css'      => 'width:170px;',
+                ),
+                'abschreibung_pro_km' => array(
+                    'title'             => __( 'Abschreibung pro km'),
+                    'type'              => 'text',
+                    'description'       => __( '' ),
+                    'desc_tip'          => false,
+                    'default'           => '',
+                    'css'      => 'width:170px;',
+                ),
+                'wartungskosten' => array(
+                    'title'             => __( 'Wartungskosten pro km'),
+                    'type'              => 'text',
+                    'description'       => __( '' ),
+                    'desc_tip'          => false,
+                    'default'           => '',
+                    'css'      => 'width:170px;',
+                ),
+                'plz1' => array(
+                    'title'             => __( 'plz1'),
+                    'type'              => 'text',
+                    'description'       => __( '' ),
+                    'desc_tip'          => false,
+                    'default'           => '',
+                    'css'      => 'width:170px;',
+                ),
+                'plz2' => array(
+                    'title'             => __( 'plz2'),
+                    'type'              => 'text',
+                    'description'       => __( '' ),
+                    'desc_tip'          => false,
+                    'default'           => '',
+                    'css'      => 'width:170px;',
+                ),
+                'entfernung_output' => array(
+                    'title'             => __( 'Entfernung'),
+                    'type'              => 'text',
+                    'description'       => __( '' ),
+                    'desc_tip'          => false,
+                    'default'           => ($this->plz1 != null && $this->plz2 != null) ? calculate_distance_and_duration($this->plz1, $this->plz2)['distance'] : '',
+                    'css'      => 'width:170px;',
+                ),
+                'fahrzeit_output' => array(
+                    'title'             => __( 'Fahtzeit'),
+                    'type'              => 'text',
+                    'description'       => __( '' ),
+                    'desc_tip'          => false,
+                    'default'           => ($this->plz1 != null && $this->plz2 != null) ? calculate_distance_and_duration($this->plz1, $this->plz2)['duration'] : '',
+                    'css'      => 'width:170px;',
+                ),
+                'kosten_output' => array(
+                    'title'             => __( 'Kosten'),
+                    'type'              => 'text',
+                    'description'       => __( '' ),
+                    'desc_tip'          => false,
+                    'default'           => ($this->plz1 != null && $this->plz2 != null) ? calculate_shipping($this->plz1, $this->plz2) : '',,
+                    'css'      => 'width:170px;',
+                ),
+                
             );
         }
 
