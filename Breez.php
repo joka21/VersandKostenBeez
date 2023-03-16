f2144ee650327b20c678e61f57fc071866d82056
jo kalenberg <team@krefeld2030.de>
2023-03-16 18:36:57 +0100

    first commit

---
+++ Breez.php
@@ -0,0 +1,16 @@
+<?php
+/*
+ * Plugin Name:       Versand_Kosten_Beez
+ * 
+ * Description:       Berechnet die Versandkosten.
+ * Version:           1.0
+ 
+ * Requires PHP:      7.2
+ * Author:           Medienwerkstatt-niederrhein
+ * Author URI:        https://author.example.com/
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Update URI:        https://example.co
+ * Text Domain:       my-basics-plugin
+ 
+ */
