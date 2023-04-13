f573ac47967add5061c1c39c43e721687e9a729e
DrAlli (Marius Martin) <35598714+DrAlliteration@users.noreply.github.com>
2023-04-13 23:06:58 +0200

    refactor: Code-Cleanup für die Hauptplugin-Datei

--- versandkosten-beez-plugin.php
+++ versandkosten-beez-plugin.php
@@ -1,22 +1,25 @@
 <?php
 /*
- * Plugin Name:       Versand Kosten Beez
- * 
- * Description:       Berechnet die Versandkosten!
- * Version:           1.0
- 
- * Requires PHP:      7.2
- * Author:            Medienwerkstatt-niederrhein
- * Author URI:        https://author.example.com/
- * License:           GPL v2 or later
- * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
- * Update URI:        https://example.co
- * Text Domain:       my-basics-plugin
+ * Plugin Name:             Versandkosten Beez
+ * Description:             Berechnet die Versandkosten und verwaltet die verfügbaren Lieferwochen!
+ * Version:                 1.0
+ * Requires PHP:            7.2
+ * Requires WordPress:      5.2
+ * Requires WooCommerce:    3.6
+ * Author:                  Medienwerkstatt-niederrhein
+ * Author URI:              https://medienwerkstatt-niederrhein.de/
+ * License:                 GPL v2 or later
+ * License URI:             https://www.gnu.org/licenses/gpl-2.0.html
  */
+
+
+// Set the plugin slugs
+const VERSANDKOSTEN_BEEZ_SHIPPING_METHOD_SLUG = 'wc-settings';
+const VERSANDKOSTEN_BEEZ_SHIPPING_MANAGEMENT_SLUG = 'Versandkosten Beez Lieferwochen Management';
 
 require_once('versandkosten-beez-availability-settings-page.php');
 
-// Include the main Versand_Kosten_Beez_Plugin class.
+// Create Plugin Main Class
 if ( ! class_exists( 'Versand_Kosten_Beez_Plugin' ) ) :
     class Versand_Kosten_Beez_Plugin {
         private Versand_Kosten_Beez_Shipping_Method $shipping_method;
@@ -34,6 +37,8 @@ if ( ! class_exists( 'Versand_Kosten_Beez_Plugin' ) ) :
         * Init the plugin
         */
         public function init() {
+
+            // Include required files
             require_once('versandkosten-beez-shipping-availability-dao.php');
             require_once('versandkosten-beez-shipping-method.php');
             require_once('versandkosten-beez-product-controller.php');
@@ -47,23 +52,25 @@ if ( ! class_exists( 'Versand_Kosten_Beez_Plugin' ) ) :
             // Init product controller
             $this->product_controller = new versandkosten_beez_product_controller();
 
-            // Set the plugin slug
-            define( 'VERSAND_KOSTEN_BEEZ_SLUG', 'wc-settings' );
-
             // Setting action for plugin
-            add_filter( 'plugin_action_links_' . plugin_basename( __FILE__ ), 'Versand_Kosten_Beez_Plugin_action_links' );
+            add_filter( 'plugin_action_links_' . plugin_basename( __FILE__ ), array($this, 'Versand_Kosten_Beez_Plugin_action_links'));
+        }
+
+        /**
+         * Add the settings link to the plugins page
+         */
+        function Versand_Kosten_Beez_Plugin_action_links( $links ) {
+            $links[] = '<a href="'. menu_page_url( VERSANDKOSTEN_BEEZ_SHIPPING_METHOD_SLUG, false ) .'&tab=shipping&section=versand-kosten-beez-shipping-method">Lieferkosten Einstellungen</a>';
+            $links[] = '<a href="'. menu_page_url( VERSANDKOSTEN_BEEZ_SHIPPING_MANAGEMENT_SLUG, false ). '">Lieferwochen verwalten</a>';
+            return $links;
         }
     }
 
     $Versand_Kosten_Beez_Plugin = new Versand_Kosten_Beez_Plugin( __FILE__ );
-    function Versand_Kosten_Beez_Plugin_action_links( $links ) {
-        $links[] = '<a href="'. menu_page_url( VERSAND_KOSTEN_BEEZ_SLUG, false ) .'&tab=shipping&section=versand-kosten-beez-shipping-method">Settings</a>';
-        return $links;
-    }
 
+    // Add the lieferwochen management page
     $shipping_availability_settings = new VersandkostenBeezAvailabilitySettingsPage();
     add_action( 'init', array($shipping_availability_settings, 'init'));
 
 endif;
-
 ?>
