f258b6ebbbd2b75ae8538b43f8b2cc0b1947e575
DrAlli (Marius Martin) <35598714+DrAlliteration@users.noreply.github.com>
2023-04-13 23:08:20 +0200

    refactor: Code-Cleanup vom Produkt-Controller

--- versandkosten-beez-product-controller.php
+++ versandkosten-beez-product-controller.php
@@ -1,13 +1,18 @@
 <?php
 
-if( !class_exists('versandkosten_beez_product_controller')){
 
+if( !class_exists('versandkosten_beez_product_controller')):
+
+    /**
+     * Class for controlling the product page
+     */
     class versandkosten_beez_product_controller{
-
         private Versand_Kosten_Beez_Shipping_Method $shipping_method;
-
         private static bool $hooks_loaded = false;
 
+        /**
+         * Construct the plugin
+         */
         public function __construct(){
             // Init shipping method
             $this->shipping_method = new Versand_Kosten_Beez_Shipping_Method();
@@ -46,12 +51,12 @@ if( !class_exists('versandkosten_beez_product_controller')){
                 ?>
                 <div id="shipping-informations-product">
                     <div>
-                        <span id="plz-output">Postleitzahl: <? echo htmlspecialchars($plz); ?></span>
+                        <span id="plz-output">Postleitzahl: <?php echo htmlspecialchars($plz); ?></span>
                         <button type="button" class="btn-popup">Postleitzahl ändern</button>
                     </div>
 
-                    <?
-                        $this->shipping_method->add_shipping_week_selection();
+                    <?php
+                        $this->add_shipping_week_selection();
                     ?>
                 </div>
 
@@ -59,7 +64,7 @@ if( !class_exists('versandkosten_beez_product_controller')){
                     <div class="plz-popup-content">
                         <form id="plz-form">
                             <label for="plz-input">Bitte geben Sie die Postleitzahl des Liefergebiets ein:</label>
-                            <input type="text" id="plz-input" name="plz" minlength="5" maxlength="5" title="Bitte geben Sie eine Postleitzahl mit Stellen ein" required>
+                            <input type="text" id="plz-input" name="plz" minlength="5" maxlength="5" required>
                             <button type="submit" class="btn-popup-submit">Speichern</button>
                         </form>
                     </div>
@@ -106,14 +111,15 @@ if( !class_exists('versandkosten_beez_product_controller')){
          * Add jQuery to footer of product page
          */
         function add_popup_jquery() {
-            //check if we are on a product page
             if(is_product()) {
                 ?>
                 <script>
                     jQuery(document).ready(function() {
                         // create hidden input for lieferwoche and lieferjahr
-                        jQuery(".cart").append('<input type="hidden" name="lieferwoche" id="lieferwoche-input-hidden">');
-                        jQuery(".cart").append('<input type="hidden" name="lieferjahr" id="lieferjahr-input-hidden">');
+                        jQuery(".cart").append('' +
+                            '<input type="hidden" name="lieferwoche" id="lieferwoche-input-hidden">' +
+                            '<input type="hidden" name="lieferjahr" id="lieferjahr-input-hidden">'
+                        );
 
                         // fill hidden input with value of selected shipping week and year
                         jQuery("#select-lieferwochen").change(function() {
@@ -151,7 +157,7 @@ if( !class_exists('versandkosten_beez_product_controller')){
                                         jQuery("#plz-popup").show();
                                     }
                                 },
-                                error: function(xhr, status, error) {
+                                error: function(xhr) {
                                     console.log(xhr.responseText);
                                 }
                             });
@@ -166,7 +172,7 @@ if( !class_exists('versandkosten_beez_product_controller')){
                         function change_postleitzahl(plz) {
                             fill_total_cost_overview(plz);
                             set_postleitzahl_output(plz);
-                            document.cookie="plz="+plz+";path=/";
+                            document.cookie="plz="+plz+";path=/;days=30";
                         }
 
                         //function to get postleitzahl cookie
@@ -227,6 +233,9 @@ if( !class_exists('versandkosten_beez_product_controller')){
             }
         }
 
+        /**
+         * Add css to product page
+         */
         function add_product_css(){
             if(is_product()){
                 //add css from css/product.css
@@ -238,31 +247,39 @@ if( !class_exists('versandkosten_beez_product_controller')){
          * Get shipping costs from Shipping Method
          */
         function get_shipping_costs() {
-            $plz = $_POST['plz'];
+            $plz = $_POST['plz'] ?? "";
             try{
+                // get shipping costs
                 $shipping_costs = $this->shipping_method->get_shipping_costs($plz);
 
+                // change shipping information of user to plz
                 $this->change_customer_info_by_plz($plz);
 
+                // return shipping costs
                 $ret = array(
                     'status' => "success",
                     'shipping_costs' => $shipping_costs
                 );
             }catch(Exception $e){
+                // return error
                 $ret = array(
                     'status' => "error",
                     'message' => $e->getMessage()
                 );
             }
-
-            echo json_encode($ret);
-            wp_die();
+            // return json and die
+            wp_die(json_encode($ret));
         }
 
         /**
          * Change shipping information of user
          */
         function change_customer_info_by_plz($plz){
+            //validate plz
+            if(!$this->shipping_method->validate_german_zip($plz)){
+                return;
+            }
+
             //change shipping information of user
             GLOBAL $woocommerce;
             $woocommerce->customer->set_shipping_postcode($plz);
@@ -274,7 +291,44 @@ if( !class_exists('versandkosten_beez_product_controller')){
                 $woocommerce->customer->set_shipping_state("DE-".$info['adminCode1']);
             }catch(Exception $e){/* do nothing*/}
 
+            // save changes
             $woocommerce->customer->save();
+        }
+
+
+        /**
+         * Adds shipping week selection html to product page
+         * @return void
+         */
+        public function add_shipping_week_selection(){
+            ?>
+            <div id="shipping-informations-week">
+                <label for="select-lieferwochen">Lieferwoche *</label>
+                <select id="select-lieferwochen">
+                    <option value="">--Bitte auswählen --</option>
+                    <?php
+                    $lieferwochen = $this->shipping_method->get_lieferwochen();
+                    foreach($lieferwochen as $lieferwoche) {
+                        $week = $lieferwoche->getWeek();
+                        $year = $lieferwoche->getYear();
+                        $start = $lieferwoche->getStart();
+                        $end = $lieferwoche->getEnd();
+                        $enabled = $lieferwoche->getEnabled();
+
+                        echo "<option data-year='$year' value='$week'";
+                        if($enabled) {
+                            echo ">KW $week ($start - $end)";
+                        } else {
+                            echo "disabled>
+                                            KW $week ($start - $end) - ausgebucht!";
+                        }
+                        echo "</option>";
+                    }
+                    ?>
+                </select>
+            </div>
+
+            <?php
         }
 
         /**
@@ -285,6 +339,6 @@ if( !class_exists('versandkosten_beez_product_controller')){
             $this->change_customer_info_by_plz($plz);
         }
     }
-}
+endif;
 
 ?>
